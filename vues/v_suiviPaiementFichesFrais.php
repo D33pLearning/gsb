9754294f2e5110ff0dd7ef700a20e587c2b54d1f
abc <fevrejeremy74@gmail.com>
2018-04-21 14:02:25 +0200

    modif avant deploiement

--- vues/v_suiviPaiementFichesFrais.php
+++ vues/v_suiviPaiementFichesFrais.php
@@ -6,35 +6,3 @@
  * @author    Jérémy FEVRE
  */
  
-?>
-<hr>
-<div class="row">
-    <div class="panel panel-info">
-        <div class="panel-heading">Descriptif des éléments hors forfait</div>
-        <table class="table table-bordered table-responsive">
-            <thead>
-                <tr>
-                    <th class="idEtat">Etat</th>
-                    <th class="montantValide">Montant</th>  
-                    <th class="libelle">Libelle</th>   
-                </tr>
-            </thead>  
-            <tbody>
-            <?php
-            foreach ($lesFiches as $uneFiche) {
-                $idEtat = $uneFiche['idEtat'];
-                $montantValide = $uneFiche['montantValide'];
-                $libelle = $uneFiche['libelle'];
-                ?>           
-                <tr>
-                    <td> <?php echo $idEtat ?></td>
-                    <td> <?php echo $montantValide ?></td>
-                    <td><?php echo $libelle ?></td>
-                </tr>
-                <?php
-            }
-            ?>
-            </tbody>  
-        </table>
-    </div>
-</div>
